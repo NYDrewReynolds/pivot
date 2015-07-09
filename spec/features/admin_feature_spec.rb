7e8df372d21e184a8bc06252ee3b5ac20d603920
Josh Cass <joshuacass@gmail.com>
2015-07-09 11:55:52 -0600

    admin tests passing

--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -1,169 +1,160 @@
-# require 'capybara/poltergeist'
-# Capybara.javascript_driver = :poltergeist
-# Capybara.default_wait_time = 5
-#
-# describe 'admin user', type: :feature do
-#
-# 	before do
-# 		@user = create(:user, first_name: 'joe', email: 'abc@example.com', password: 'asdf', password_confirmation: 'asdf', role:'admin')
-#     create(:restaurant, name: "Pizza Palace", cuisine: "food")
-# 		create(:restaurant, name: "Jorges Garden", cuisine: "mexican")
-# 		create(:restaurant, name: "Sergios", cuisine: "italian")
-#     visit '/'
-#     fill_in 'email', with: "#{@user.email}"
-#     fill_in 'password', with: "#{@user.password}"
-#     click_on 'login'
-# 	end
-#
-# 	it 'has a role of admin' do
-# 		visit '/'
-# 		expect(page).to have_content("Admin Dashboard")
-# 	end
-#
-# 	it 'is redirected to an admin dashboard upon login' do
-# 		click_on 'Logout'
-# 		expect(current_path).to eq root_path
-# 		fill_in 'email', with: "#{@user.email}"
-# 		fill_in 'password', with: "#{@user.password}"
-# 		click_on 'login'
-# 		expect(page).to have_content("Administrator")
-# 		expect(current_path).to eq admin_path
-# 		expect(page).to have_content "Site Administrator Dashboard"
-# 	end
-#
-# describe 'admin dashboard' do
-#
-# 		it 'has link to create new items' do
-# 		  visit admin_path
-# 			expect(page).to have_content('Site Administrator Dashboard')
-# 			expect(page).to have_content('Create A New Menu Item')
-# 		end
-#
-# 		it 'has link to manage users' do
-# 			visit admin_path
-# 			expect(page).to have_content('View Current Users')
-# 		end
-#
-# 		it 'has link to manage orders' do
-# 			visit admin_path
-# 			expect(page).to have_content('View Current Orders')
-# 		end
-# 	end
-#
-# 	it 'can create item listings' do
-# 		small_plates_category = create(:category, title: 'Small Plates')
-# 		visit '/admin'
-# 		click_on('Create A New Menu Item')
-# 		expect(page).to have_content("Create New Item")
-# 		fill_in 'Title', with: "Test Item"
-# 		fill_in 'Description', with: "Test Description"
-# 		fill_in 'Price', with: '19.22'
-#     find(:css, ".category_checkbox").set(true)
-# 		click_on('Create Item')
-# 		expect(page).to have_content("Your item has been successfully added to the menu!")
-# 	end
-#
-# 	it 'can edit item listings' do
-# 		small_plates_category = create(:category, title: 'Small Plates')
-# 		create(:item, id: 1, title: 'Second Food', categories: [small_plates_category])
-# 		visit 'admin/items/1/edit'
-# 		expect(page).to have_content("You are currently editing")
-# 		fill_in "item_title", with: "Edited Item"
-# 		fill_in "item_description", with: "Edited Description"
-# 		fill_in "item_price", with: "12.00"
-# 		click_on('Save Changes')
-# 		expect(page).to have_content("Your item has been successfully updated!")
-# 	end
-#
-# 	it 'can create named categories for items' do
-# 		visit '/admin'
-# 		click_on('Create A New Category')
-# 		expect(page).to have_content("Create New Category")
-# 		fill_in "category_title", with: "New Category"
-# 		click_on('Create Category')
-# 		expect(page).to have_content("Your category has been successfully created!")
-# 	end
-#
-# 	it 'can assign items to categories' do
-# 		small_plates_category = create(:category, title: 'Small Plates')
-# 		create(:item, id: 1, title: 'Second Food', categories: [small_plates_category])
-# 		visit '/admin/items/1/edit'
-# 		find(:css, ".category_checkbox").set(true)
-# 		click_on("Save Changes")
-# 		visit '/admin/items/1'
-# 		expect(page).to have_content("Small Plates")
-# 	end
-#
-# 	it 'can remove items from categories' do
-# 		small_plates_category = create(:category, title: 'Small Plates')
-# 		create(:item, id: 1, title: 'Second Food', categories: [small_plates_category])
-# 		visit '/admin/items/1/edit'
-# 		find(:css, ".category_checkbox").set(false)
-# 		checkbox = find(".category_checkbox")
-# 		expect(checkbox).to_not be_checked
-# 		click_on("Save Changes")
-# 		visit '/admin/items/1'
-# 		expect(page).to_not have_content("Small Plates")
-# 	end
-#
-# 	it 'can retire items from being sold' do
-# 		small_plates_category = create(:category, title: 'Small Plates')
-# 		create(:item, id: 1, title: 'Second Food', categories: [small_plates_category])
-# 		visit '/admin/items/1/edit'
-# 		expect(page).to have_content("Active")
-# 		click_on 'Save Changes'
-# 		expect(page).to have_content("Your item has been successfully updated!")
-# 	end
-#
-# 	it 'can see retired items only as an admin' do
-# 		small_plates_category = create(:category, title: 'Small Plates')
-# 		item = create(:item, id: 1, title: 'Second Food', categories: [small_plates_category], active: 'false')
-# 		visit '/admin/items/1/edit'
-# 		expect(page).to have_content("Active")
-# 		click_on 'Save Changes'
-# 		expect(page).to have_content("Your item has been successfully updated!")
-# 		click_link 'Return to Your Admin Dashboard'
-# 		expect(page).to have_content"#{item.active}"
-# 		expect(page).to have_content"#{item.id}"
-# 	end
-#
-#   it "can see all users" do
-#     visit '/admin'
-# 		click_on 'Create New User or Administrator'
-# 		expect(current_path).to eq new_admin_user_path
-#     expect(page).to have_content("Create A New User or Administrator")
-#   end
-#
-# 	it "can create a new user with admin role" do
-# 		visit '/admin/users/new'
-# 		fill_in 'First name', with: 'abc'
-# 		fill_in 'Last name', with: 'poptart'
-# 		fill_in 'Email', with: 'tartkins@example.com'
-# 		fill_in 'Password', with: "abc123"
-# 		fill_in 'Password confirmation', with: "abc123"
-# 		select 'admin', from: 'user_role'
-# 		click_on 'Create New User'
-# 		expect(current_path).to eq admin_users_path
-# 		expect(User.last.role).to eq 'admin'
-# 	end
-#
-#   it "can see all existing users" do
-#     visit "/admin/users"
-#     expect(page).to have_content("Current List of Caussa Users")
-#   end
-#
-# 	it "can modify an existing user's role" do
-# 		nonadmin_user = create(:user, first_name: 'jojo', email: 'jojojo@example.com', password: 'asdf', password_confirmation: 'asdf', role:'user')
-# 		visit '/admin'
-# 		click_on 'View Current Users'
-# 		expect(current_path).to eq admin_users_path
-# 		click_on('user')
-# 		expect(current_path).to eq edit_admin_user_path(nonadmin_user)
-# 		select 'Admin', from: 'user_role'
-# 		click_on 'Save Changes'
-# 		expect(User.last.role).to eq 'Admin'
-# 	end
-#
+require 'capybara/poltergeist'
+Capybara.javascript_driver = :poltergeist
+Capybara.default_wait_time = 5
+
+describe 'admin user', type: :feature do
+
+	before do
+    @restaurant = create(:restaurant, name: "Pizza Palace", cuisine: "food")
+		create(:restaurant, name: "Jorges Garden", cuisine: "mexican")
+		create(:restaurant, name: "Sergios", cuisine: "italian")
+    category = Category.create(title: 'Small Plates', restaurant_id: @restaurant.id)
+		@user = create(:user, first_name: 'joe', email: 'abc@example.com', password: 'asdf', password_confirmation: 'asdf', restaurant_id: @restaurant.id, role:'admin')
+    @item = Item.create(title: 'Second Food', category_ids: category.id, description: "foodn' shit", price: 10, restaurant_id: @restaurant.id)
+    visit '/'
+    fill_in 'email', with: "#{@user.email}"
+    fill_in 'password', with: "#{@user.password}"
+    click_on 'login'
+	end
+
+	it 'has a role of admin' do
+		visit '/'
+		expect(page).to have_content("Admin Dashboard")
+	end
+
+	it 'is redirected to an admin dashboard upon login' do
+		click_on 'Logout'
+		expect(current_path).to eq root_path
+		fill_in 'email', with: "#{@user.email}"
+		fill_in 'password', with: "#{@user.password}"
+		click_on 'login'
+		expect(page).to have_content("Administrator")
+		expect(current_path).to eq restaurant_admin_dashboard_index_path(@restaurant)
+		expect(page).to have_content "Site Administrator Dashboard"
+	end
+
+describe 'admin dashboard' do
+
+		it 'has link to create new items' do
+		  visit restaurant_admin_dashboard_index_path(@restaurant)
+			expect(page).to have_content('Site Administrator Dashboard')
+			expect(page).to have_content('Create A New Menu Item')
+		end
+
+		it 'has link to manage users' do
+			visit restaurant_admin_dashboard_index_path(@restaurant)
+			expect(page).to have_content('View Current Users')
+		end
+
+		it 'has link to manage orders' do
+			visit restaurant_admin_dashboard_index_path(@restaurant)
+			expect(page).to have_content('View Current Orders')
+		end
+	end
+
+	it 'can create item listings' do
+		visit restaurant_admin_dashboard_index_path(@restaurant)
+		click_on('Create A New Menu Item')
+		expect(page).to have_content("Create New Item")
+		fill_in 'Title', with: "Test Item"
+		fill_in 'Description', with: "Test Description"
+		fill_in 'Price', with: '19.22'
+    find(:css, ".category_checkbox").set(true)
+		click_on('Create Item')
+		expect(page).to have_content("Your item has been successfully added to the menu!")
+	end
+
+	it 'can edit item listings' do
+		visit edit_restaurant_admin_item_path(@restaurant, @item)
+		expect(page).to have_content("You are currently editing")
+		fill_in "item_title", with: "Edited Item"
+		fill_in "item_description", with: "Edited Description"
+		fill_in "item_price", with: "12.00"
+		click_on('Save Changes')
+		expect(page).to have_content("Your item has been successfully updated!")
+	end
+
+	it 'can create named categories for items' do
+		visit restaurant_admin_dashboard_index_path(@restaurant)
+		click_on('Create A New Category')
+		expect(page).to have_content("Create New Category")
+		fill_in "category_title", with: "New Category"
+		click_on('Create Category')
+		expect(page).to have_content("Your category has been successfully created!")
+	end
+
+	it 'can assign items to categories' do
+		visit edit_restaurant_admin_item_path(@restaurant, @item)
+		find(:css, ".category_checkbox").set(true)
+		click_on("Save Changes")
+		visit restaurant_admin_item_path(@restaurant, @item)
+		expect(page).to have_content("Small Plates")
+	end
+
+	it 'can remove items from categories' do
+		visit edit_restaurant_admin_item_path(@restaurant, @item)
+		find(:css, ".category_checkbox").set(false)
+		checkbox = find(".category_checkbox")
+		expect(checkbox).to_not be_checked
+		click_on("Save Changes")
+		visit restaurant_admin_item_path(@restaurant, @item)
+		expect(page).to_not have_content("Small Plates")
+	end
+
+	it 'can retire items from being sold' do
+		visit edit_restaurant_admin_item_path(@restaurant, @item)
+		expect(page).to have_content("Active")
+		click_on 'Save Changes'
+		expect(page).to have_content("Your item has been successfully updated!")
+	end
+
+	it 'can see retired items only as an admin' do
+		visit edit_restaurant_admin_item_path(@restaurant, @item)
+		expect(page).to have_content("Active")
+		click_on 'Save Changes'
+		expect(page).to have_content("Your item has been successfully updated!")
+		click_link 'Admin Dashboard'
+		expect(page).to have_content"#{@item.active}"
+		expect(page).to have_content"#{@item.id}"
+	end
+
+  it "can see all restaurant users" do
+    restaurant_admin_dashboard_index_path(@restaurant)
+		click_on 'Create New User or Administrator'
+		expect(current_path).to eq new_restaurant_admin_user_path(@restaurant)
+    expect(page).to have_content("Create A New User or Administrator")
+  end
+
+	it "can create a new user with admin role" do
+		visit new_restaurant_admin_user_path(@restaurant)
+		fill_in 'First name', with: 'abc'
+		fill_in 'Last name', with: 'poptart'
+		fill_in 'Email', with: 'tartkins@example.com'
+		fill_in 'Password', with: "abc123"
+		fill_in 'Password confirmation', with: "abc123"
+		select 'admin', from: 'user_role'
+		click_on 'Create New User'
+		expect(current_path).to eq restaurant_admin_users_path(@restaurant)
+		expect(User.last.role).to eq 'admin'
+	end
+
+  it "can see all existing users" do
+    visit restaurant_admin_users_path(@restaurant)
+    expect(page).to have_content("Current List of #{@restaurant.name} Users")
+  end
+
+	it "can modify an existing user's role" do
+		nonadmin_user = create(:user, first_name: 'jojo', email: 'jojojo@example.com', password: 'asdf', password_confirmation: 'asdf', role:'user', restaurant_id: @restaurant.id)
+    restaurant_admin_dashboard_index_path(@restaurant)
+		click_on 'View Current Users'
+		expect(current_path).to eq restaurant_admin_users_path(@restaurant)
+		click_on('user')
+		expect(current_path).to eq edit_restaurant_admin_user_path(@restaurant, nonadmin_user)
+		select 'Admin', from: 'user_role'
+		click_on 'Save Changes'
+		expect(User.last.role).to eq 'Admin'
+	end
+
 # describe 'admin order dashboard' do
 #
 # 	before do
@@ -290,4 +281,4 @@
 #
 # end
 #
-# end
+end
