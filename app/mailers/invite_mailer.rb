74a0c27d655a71af6f532092a439940091e09fb2
Josh Cass <joshuacass@gmail.com>
2015-07-16 14:56:10 -0600

    changed from address

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,6 +3,6 @@ class InviteMailer < ApplicationMailer
   def invite_email(email_address, owner, restaurant)
     @restaurant = restaurant
     @owner = owner
-    mail(to: email_address, subject: "Join my staff!", from: "#{owner.first_name}@#{restaurant.name}.com")
+    mail(to: email_address, subject: "Join my staff!", from: "#{owner.first_name}@fud.com")
   end
 end
