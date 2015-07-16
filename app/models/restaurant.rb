85ef4eb026bd78bc6dbfce2918a552cab97abb3f
Josh Cass <joshuacass@gmail.com>
2015-07-16 15:09:16 -0600

    slug name fix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,7 @@ class Restaurant < ActiveRecord::Base
   has_many :categories
   has_many :user_staff_roles
   has_many :users, through: :user_staff_roles
+  before_save :set_slug
 
   friendly_id :slug_candidates, use: :slugged
 
@@ -12,12 +13,15 @@ class Restaurant < ActiveRecord::Base
   validates :cuisine, presence: true
 
   def slug_candidates
-    [:slug_name,
-     :name]
+    [:slug_name, :name]
   end
 
   def should_generate_new_friendly_id?
     slug_name_changed? || super
+  end
+
+  def set_slug
+    self.slug_name = self.name unless self.slug_name
   end
 
 end
